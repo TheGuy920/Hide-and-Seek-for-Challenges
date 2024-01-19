1e71c7dff11b15a9283de04decbceac0364dedfc
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-01-19 00:05:01 -0500

    a

--- Scripts/CustomGame/SpectateBlock.lua
+++ Scripts/CustomGame/SpectateBlock.lua
@@ -55,10 +55,9 @@ end
 
 function SpectateBlock.client_onUpdate( self, deltaTime )
     if self.mode == Modes.Follow then
-        if not self.target then
-            self.target = sm.player.getAllPlayers()[self.spectateIndex]
+        if self.target then       
+            sm.camera.setPosition(self.target.character.worldPosition)
         end
-        sm.camera.setPosition(self.target.character.worldPosition)
     else
         self.target = nil
     end
@@ -67,23 +66,41 @@ end
 function SpectateBlock.client_onAction( self, input, active )
 
     if active then
+        local players = sm.player.getAllPlayers()
+
         if input == Controls.W then self.wdown = true end 
         if input == Controls.S then self.sdown = true end
         if input == Controls.A then self.adown = true end
         if input == Controls.D then self.ddown = true end
 
-        if input == Controls.Mouse1 then
-            self.spectateIndex = self.spectateIndex + 1
-            if self.spectateIndex > #sm.player.getAllPlayers() - 1 then
-                self.spectateIndex = 0
-            end
-        elseif input == Controls.Mouse2 then
-            self.spectateIndex = self.spectateIndex - 1
-            if self.spectateIndex < 0 then
-                self.spectateIndex = #sm.player.getAllPlayers() - 1
+        if #players > 1 then
+            if input == Controls.Mouse1 then
+                self.spectateIndex = self.spectateIndex + 1
+                ::M1Check::
+                if self.spectateIndex > #players - 1 then
+                    self.spectateIndex = 0
+                end
+
+                self.target = players[self.spectateIndex+1]
+                if self.target == sm.localPlayer.getPlayer() then
+                    self.spectateIndex = self.spectateIndex + 1
+                    goto M1Check
+                end
+            elseif input == Controls.Mouse2 then
+
+                self.spectateIndex = self.spectateIndex - 1
+                ::M2Check::
+                if self.spectateIndex < 0 then
+                    self.spectateIndex = #players - 1
+                end
+
+                self.target = players[self.spectateIndex+1]
+                if self.target == sm.localPlayer.getPlayer() then
+                    self.spectateIndex = self.spectateIndex - 1
+                    goto M2Check
+                end
             end
         end
-
     else
         if input == Controls.W then self.wdown = false end 
         if input == Controls.S then self.sdown = false end
